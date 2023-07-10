c796f8155b9545337bd6d512cc2e6a9538fd9c31
rocady21 <68871996+rocady21@users.noreply.github.com>
2023-07-10 15:26:13 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generateExcuse() {
+  const whoPosition = getRandomIntInclusive(0, who.length - 1);
+  const actionPosition = getRandomIntInclusive(0, action.length - 1);
+  const whatPosition = getRandomIntInclusive(0, what.length - 1);
+  const whenPosition = getRandomIntInclusive(0, when.length - 1);
+
+  return (
+    who[whoPosition] +
+    " " +
+    action[actionPosition] +
+    " " +
+    what[whatPosition] +
+    "" +
+    when[whenPosition]
+  );
+}
+
+function onload() {
+  const Excuse = generateExcuse();
+  const excuseDocument = document?.getElementById("Excuse");
+
+  return (excuseDocument.innerHTML = Excuse);
+}
+
+onload();
